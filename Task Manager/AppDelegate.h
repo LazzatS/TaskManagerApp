3564ba3bab88b60d5ad21da1f3e34f418db35b45
Lazzatios <lseiilova@nu.edu.kz>
2021-07-13 00:59:09 +0600

    Initial Commit

---
+++ Task Manager/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Task Manager
+//
+//  Created by Lazzat Seiilova on 13.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Task Manager/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Task Manager
+//
+//  Created by Lazzat Seiilova on 13.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
