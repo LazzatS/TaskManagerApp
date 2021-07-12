30cd6f572d98bfcacb8828caf83cdf4323c5c4cf
Lazzatios <lseiilova@nu.edu.kz>
2021-07-13 01:45:32 +0600

    docs: add LoginViewController .h and .m files

---
+++ Task Manager/LoginViewController/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  Task Manager
+//
+//  Created by Lazzat Seiilova on 13.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Task Manager/LoginViewController/LoginViewController.m
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.m
+//  Task Manager
+//
+//  Created by Lazzat Seiilova on 13.07.2021.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
